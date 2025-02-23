cae74cbee4a1412b3f9011eb9293def87be3973d
arthur de andrade <arthurderochaa@gmail.com>
2025-02-23 14:17:24 -0300

    crud in service layer

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async create(createUserDto: Prisma.UserCreateInput) {
+    return this.databaseService.user.create({
+      data: createUserDto,
+    });
+  }
+
+  async findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role)
+      return this.databaseService.user.findMany({
+        where: {
+          role,
+        },
+      });
+    return this.databaseService.user.findMany();
+  }
+
+  async findOne(id: number) {
+    return this.databaseService.user.findUnique({
+      where:{
+        id
+      }
+    })
+  }
+
+  async update(id: number, updateEmployeeDto: Prisma.UserUpdateInput) {
+    return this.databaseService.user.update({
+      where:{
+        id,
+      },
+      data: updateEmployeeDto
+    })
+  }
+
+  async remove(id: number) {
+    return this.databaseService.user.delete({
+      where:{
+        id,
+      }
+    })
+  }
+
+}
